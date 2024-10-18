2c4366af50aeb8975ea26625b8e37fd7f5c02731
NSOLAf1 <nsolaf1@gmail.com>
2024-10-18 14:17:35 +0600

    Module

---
+++ Module/Module_4.sql
@@ -0,0 +1,109 @@
+--Module 4/7 50%
+
+--HINT 
+ --   Group BY
+ --   Having Count()
+ --   Distinct
+
+ -----------------------------------------------------------------------------------------------------------
+
+ВАША ЗАДАЧА: 
+
+Напишите решение для отображения id клиентов из таблицы клиентов, которые купили все продукты в таблице продуктов.
+
+Верните таблицу результатов в любом порядке.
+
+-----------------------------------------------------------------------------------------------------------
+
+Table: Customer
+    +-------------+---------+
+    | Column Name | Type    |
+    +-------------+---------+
+    | customer_id | int     |
+    | product_key | int     |
+    +-------------+---------+
+
+Эта таблица может содержать повторяющиеся строки. 
+customer_id не равен нулю.
+product_key - это foreign key(внешний ключ) таблицы Product.
+ 
+
+Table: Product
+    +-------------+---------+
+    | Column Name | Type    |
+    +-------------+---------+
+    | product_key | int     |
+    +-------------+---------+
+product_key - это primary key (столбец с уникальными значениями) для этой таблицы.
+ 
+
+-----------------------------------------------------------------------------------------------------------
+Формат результата приведен в следующем примере.
+
+
+Example 1:
+
+Input: 
+Customer table:
+    +-------------+-------------+
+    | customer_id | product_key |
+    +-------------+-------------+
+    | 1           | 5           |
+    | 2           | 6           |
+    | 3           | 5           |
+    | 3           | 6           |
+    | 1           | 6           |
+    +-------------+-------------+
+
+Product table:
+    +-------------+
+    | product_key |
+    +-------------+
+    | 5           |
+    | 6           |
+    +-------------+
+
+
+Output: 
+    +-------------+
+    | customer_id |
+    +-------------+
+    | 1           |
+    | 3           |
+    +-------------+
+
+Объяснение: 
+Клиенты, купившие все продукты (5 и 6), являются клиентами с идентификаторами 1 и 3.
+
+
+
+-----------------------------------------------------------------------------------------------------------
+--Query for creating the following Tables and Adding data
+-- Create Product table
+CREATE TABLE Product (
+    product_key INT PRIMARY KEY
+);
+
+-- Create Customer table
+CREATE TABLE Customer (
+    customer_id INT NOT NULL,
+    product_key INT,
+    FOREIGN KEY (product_key) REFERENCES Product(product_key)
+);
+
+-- Insert data into Product table
+INSERT INTO Product (product_key)
+VALUES
+(5),
+(6);
+
+
+-- Insert data into Customer table
+INSERT INTO Customer (customer_id, product_key)
+VALUES
+(1, 5),
+(2, 6),
+(3, 5),
+(3, 6),
+(1, 6);
+
---
+++ Module/Module_5.sql
@@ -0,0 +1,110 @@
+--Module 5/7  15%
+
+--HINT
+   -- LEFT JOIN
+
+-----------------------------------------------------------------------------------------------------------
+
+ВАША ЗАДАЧА: 
+
+Напишите решение, позволяющее отображать first name, lastname, city и state каждого человека в таблице Person. 
+Если в таблице Address отсутствует адрес с id пользователя, вместо него укажите значение null.
+
+Верните таблицу результатов в любом порядке.
+
+-----------------------------------------------------------------------------------------------------------
+
+Table: Person
+    +-------------+---------+
+    | Column Name | Type    |
+    +-------------+---------+
+    | personId    | int     |
+    | lastName    | varchar |
+    | firstName   | varchar |
+    +-------------+---------+
+personId - это первичный ключ (столбец с уникальными значениями) для этой таблицы.
+Эта таблица содержит информацию об идентификаторах некоторых людей, их именах и фамилиях.
+
+
+Table: Address
+    +-------------+---------+
+    | Column Name | Type    |
+    +-------------+---------+
+    | addressId   | int     |
+    | personId    | int     |
+    | city        | varchar |
+    | state       | varchar |
+    +-------------+---------+
+
+addressId - это первичный ключ (столбец с уникальными значениями) для этой таблицы.
+Каждая строка этой таблицы содержит информацию о городе и штате одного человека с идентификатором = PersonID.
+
+-----------------------------------------------------------------------------------------------------------
+
+Формат результата приведен в следующем примере.
+
+Input: 
+Person table:
+    +----------+----------+-----------+
+    | personId | lastName | firstName |
+    +----------+----------+-----------+
+    | 1        | Wang     | Allen     |
+    | 2        | Alice    | Bob       |
+    +----------+----------+-----------+
+
+Address table:
+    +-----------+----------+---------------+------------+
+    | addressId | personId | city          | state      |
+    +-----------+----------+---------------+------------+
+    | 1         | 2        | New York City | New York   |
+    | 2         | 3        | Bishkek       | California |
+    +-----------+----------+---------------+------------+
+
+Output: 
+    +-----------+----------+---------------+----------+
+    | firstName | lastName | city          | state    |
+    +-----------+----------+---------------+----------+
+    | Allen     | Wang     | Null          | Null     |
+    | Bob       | Alice    | New York City | New York |
+    +-----------+----------+---------------+----------+
+
+Объяснение: 
+В адресной таблице нет адреса для personId = 1, поэтому мы возвращаем значение null в их городе и штате.
+адрес Id = 1 содержит информацию об адресе personId = 2.
+
+
+-----------------------------------------------------------------------------------------------------------
+--Query for creating the following Tables and Adding data
+--для создания следующих таблиц и добавления данных
+
+-- Create Person table
+CREATE TABLE Person (
+    personId INT PRIMARY KEY,
+    lastName VARCHAR(255),
+    firstName VARCHAR(255)
+);
+
+-- Create Address table
+CREATE TABLE Address (
+    addressId INT PRIMARY KEY,
+    personId INT,
+    city VARCHAR(255),
+    state VARCHAR(255),
+    FOREIGN KEY (personId) REFERENCES Person(personId)
+);
+
+-- Insert data into Person table
+INSERT INTO Person (personId, lastName, firstName)
+VALUES
+(1, 'Wang', 'Allen'),
+(2, 'Alice', 'Bob');
+
+
+-- Insert data into Address table
+INSERT INTO Address (addressId, personId, city, state)
+VALUES
+(1, 2, 'New York City', 'New York'),
+(2, 3, 'Bishkek', 'California');
+
+
+-----------------------------------------------------------------------------------------------------------
